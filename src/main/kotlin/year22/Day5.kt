6367b2e86f16b11d269a87b3a3e311a81a418d44
jameshc2001 <jameshcrofts@gmail.com>
2023-10-02 17:15:45 +0100

    More Day 5 bits

--- src/main/kotlin/year22/Day5.kt
+++ src/main/kotlin/year22/Day5.kt
@@ -3,6 +3,15 @@ package year22
 class Day5 {
 
     data class SupplyStacks(val stacks: Map<Int, ArrayDeque<Char>>) {
+        fun performMove(move: String, grouped: Boolean = false) {
+            val (amount, from, to) = "[0-9]+".toRegex().findAll(move).toList().map { it.value.toInt() }
+            val cratesToMove = (0..<amount).mapNotNull { stacks[from]?.removeLast() }
+            val orderedCrates = if (grouped) cratesToMove.reversed() else cratesToMove
+            orderedCrates.forEach { stacks[to]?.addLast(it) }
+        }
+
+        fun topWord(): String  = stacks.values.map { it.last() }.joinToString("")
+
         companion object {
             fun fromString(input: String): SupplyStacks {
                 val lines = input.lines()
@@ -16,17 +25,29 @@ class Day5 {
                 val supplyStack = SupplyStacks(stackToIndex.keys.associateWith { ArrayDeque() })
                 lines
                     .dropLast(1)
+                    .reversed()
                     .forEach { line ->
                         stackToIndex.forEach { (stack, index) ->
                             val maybeCrate = line.getOrNull(index)
                             if (maybeCrate?.isLetter() == true) {
-                                supplyStack.stacks[stack]!!.add(maybeCrate)
+                                supplyStack.stacks[stack]!!.addLast(maybeCrate)
                             }
                         }
                     }
 
                 return supplyStack
             }
+        }
+    }
+
+    companion object {
+        fun runMovesAndGetTopWord(input: String, grouped: Boolean = false): String {
+            val (supplyStacksStart, moves) = input.replace("\r", "").split("\n\n")
+            val supplyStacks = SupplyStacks.fromString(supplyStacksStart)
+            moves
+                .lines()
+                .forEach { supplyStacks.performMove(it, grouped) }
+            return supplyStacks.topWord()
         }
     }
 
--- src/test/kotlin/year22/Day5Test.kt
+++ src/test/kotlin/year22/Day5Test.kt
@@ -10,12 +10,74 @@ class Day5Test {
         val supplyStacks = Day5.SupplyStacks.fromString(sampleInput.substringBefore("\n\n"))
         val expectedSupplyStacks = Day5.SupplyStacks(
             stacks = mapOf(
-                1 to ArrayDeque(listOf('N', 'Z')),
-                2 to ArrayDeque(listOf('D', 'C', 'M')),
+                1 to ArrayDeque(listOf('Z', 'N')),
+                2 to ArrayDeque(listOf('M', 'C', 'D')),
                 3 to ArrayDeque(listOf('P')),
             )
         )
         assertThat(supplyStacks).isEqualTo(expectedSupplyStacks)
+    }
+
+    @Test
+    fun `can perform move on supply stacks`() {
+        val supplyStacks = Day5.SupplyStacks.fromString(sampleInput.substringBefore("\n\n"))
+        val expectedSupplyStacks = Day5.SupplyStacks(
+            stacks = mapOf(
+                1 to ArrayDeque(listOf('Z', 'N', 'D')),
+                2 to ArrayDeque(listOf('M', 'C')),
+                3 to ArrayDeque(listOf('P')),
+            )
+        )
+        supplyStacks.performMove("move 1 from 2 to 1")
+        assertThat(supplyStacks).isEqualTo(expectedSupplyStacks)
+    }
+
+    @Test
+    fun `can get word formed from characters at top of each stack`() {
+        val supplyStacks = Day5.SupplyStacks.fromString(sampleInput.substringBefore("\n\n"))
+        assertThat(supplyStacks.topWord()).isEqualTo("NDP")
+    }
+
+    @Test
+    fun `can get answer for part 1 using sample input`() {
+        assertThat(Day5.runMovesAndGetTopWord(sampleInput)).isEqualTo("CMZ")
+    }
+
+    @Test
+    fun `can get answer for part 1 using question input`() {
+        val input = Day5::class.java.getResourceAsStream("/year22/day5.txt")!!.bufferedReader().readText()
+        assertThat(Day5.runMovesAndGetTopWord(input)).isEqualTo("LJSVLTWQM")
+    }
+
+    @Test
+    fun `can perform grouped move on supply stacks`() {
+        val supplyStacks = Day5.SupplyStacks(
+            stacks = mapOf(
+                1 to ArrayDeque(listOf('Z', 'N', 'D')),
+                2 to ArrayDeque(listOf('M', 'C')),
+                3 to ArrayDeque(listOf('P')),
+            )
+        )
+        val expectedSupplyStacks = Day5.SupplyStacks(
+            stacks = mapOf(
+                1 to ArrayDeque(listOf()),
+                2 to ArrayDeque(listOf('M', 'C')),
+                3 to ArrayDeque(listOf('P', 'Z', 'N', 'D')),
+            )
+        )
+        supplyStacks.performMove("move 3 from 1 to 3", true)
+        assertThat(supplyStacks).isEqualTo(expectedSupplyStacks)
+    }
+
+    @Test
+    fun `can get answer for part 2 using sample input`() {
+        assertThat(Day5.runMovesAndGetTopWord(sampleInput, true)).isEqualTo("MCD")
+    }
+
+    @Test
+    fun `can get answer for part 2 using question input`() {
+        val input = Day5::class.java.getResourceAsStream("/year22/day5.txt")!!.bufferedReader().readText()
+        assertThat(Day5.runMovesAndGetTopWord(input, true)).isEqualTo("BRQWDBBJM")
     }
 
     private val sampleInput = """
