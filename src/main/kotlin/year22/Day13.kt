eb2312d116b38ab2e7ac78035b236ce9a6ab8190
jameshc2001 <jameshcrofts@gmail.com>
2023-10-11 22:14:28 +0100

    Completed Day 13 part 1

--- src/main/kotlin/year22/Day13.kt
+++ src/main/kotlin/year22/Day13.kt
@@ -1,29 +1,30 @@
 package year22
 
 import year22.Day13.PacketValue.Integer
-import year22.Day13.PacketValue.PacketList
+import year22.Day13.PacketValue.Packet
+import kotlin.math.min
 
 class Day13 {
 
     sealed class PacketValue {
         data class Integer(val value: Int): PacketValue()
-        data class PacketList(val list: MutableList<PacketValue>): PacketValue() {
+        data class Packet(val list: MutableList<PacketValue>): PacketValue() {
             constructor(vararg values: PacketValue): this(values.toMutableList())
             fun add(value: PacketValue) = list.add(value)
         }
     }
 
     companion object {
-        fun parseList(input: String): PacketList {
-            var currentList = PacketList()
-            val stack = ArrayDeque<PacketList>()
+        fun parsePacket(input: String): Packet {
+            var currentList = Packet()
+            val stack = ArrayDeque<Packet>()
             val trimmed = input.drop(1).dropLast(1)
 
             trimmed.forEachIndexed { index, c ->
                 when {
                     c == '[' -> {
                         stack.addLast(currentList)
-                        currentList = PacketList()
+                        currentList = Packet()
                     }
                     c == ']' -> {
                         val newList = currentList
@@ -31,7 +32,7 @@ class Day13 {
                         currentList.add(newList)
                     }
                     c.isDigit() && (index == 0 || (trimmed[index - 1] in listOf('[', ','))) -> {
-                        currentList.add(trimmed.getIntegerAt(index))
+                        currentList.add(trimmed.getIntegerStartingAt(index))
                     }
                 }
             }
@@ -39,11 +40,45 @@ class Day13 {
             return currentList
         }
 
-        private fun String.getIntegerAt(index: Int): Integer {
+        private fun String.getIntegerStartingAt(index: Int): Integer {
             var end = index
-            while (this[end].isDigit()) end++
+            while (end < this.length && this[end].isDigit()) end++
             return Integer(substring(index, end).toInt())
         }
+
+        fun packetsAreInOrder(left: Packet, right: Packet): Boolean = packetsAreInOrderRecursive(left, right)!!
+
+        private fun packetsAreInOrderRecursive(left: Packet, right: Packet): Boolean? {
+            val lList = left.list
+            val rList = right.list
+            val smallestLength = min(lList.size, rList.size)
+            (0..<smallestLength).forEach { index ->
+                val lValue = lList[index]
+                val rValue = rList[index]
+                when {
+                    lValue is Integer && rValue is Integer -> {
+                        if (lValue.value < rValue.value) return true
+                        if (lValue.value > rValue.value) return false
+                    }
+                    lValue is Packet && rValue is Packet -> packetsAreInOrderRecursive(lValue, rValue)?.let { return it }
+                    lValue is Packet && rValue is Integer -> packetsAreInOrderRecursive(lValue, Packet(rValue))?.let { return it }
+                    lValue is Integer && rValue is Packet -> packetsAreInOrderRecursive(Packet(lValue), rValue)?.let { return it }
+                }
+            }
+            if (lList.size < rList.size) return true
+            if (lList.size > rList.size) return false
+            return null
+        }
+
+        fun sumOfIndicesOfInOrderPackets(input: String): Int = input
+            .replace("\r", "")
+            .split("\n\n")
+            .asSequence()
+            .map { it.split("\n") }
+            .map { (left, right) -> parsePacket(left) to parsePacket(right) }
+            .mapIndexed { index, (left, right) -> index to packetsAreInOrder(left, right) }
+            .filter { (_, inOrder) -> inOrder }
+            .sumOf { (index, _) -> index + 1 }
     }
 
 }
--- src/test/kotlin/year22/Day13Test.kt
+++ src/test/kotlin/year22/Day13Test.kt
@@ -3,22 +3,22 @@ package year22
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 import year22.Day13.PacketValue.Integer
-import year22.Day13.PacketValue.PacketList
+import year22.Day13.PacketValue.Packet
 
 class Day13Test {
 
     @Test
-    fun `can parse a single list`() {
-        val list = Day13.parseList("[1,[2,[3,[4,[5,6,7]]]],8,9,[10]]")
-        val expected = PacketList(
+    fun `can parse a single packet`() {
+        val list = Day13.parsePacket("[1,[2,[3,[4,[5,6,7]]]],8,9,[10]]")
+        val expected = Packet(
             Integer(1),
-            PacketList(
+            Packet(
                 Integer(2),
-                PacketList(
+                Packet(
                     Integer(3),
-                    PacketList(
+                    Packet(
                         Integer(4),
-                        PacketList(
+                        Packet(
                             Integer(5),
                             Integer(6),
                             Integer(7),
@@ -28,10 +28,63 @@ class Day13Test {
             ),
             Integer(8),
             Integer(9),
-            PacketList(
+            Packet(
                 Integer(10)
             )
         )
         assertThat(list).isEqualTo(expected)
     }
+
+    @Test
+    fun `can compare simple packets`() {
+        val left = Day13.parsePacket("[1,1,3,1,1]")
+        val right = Day13.parsePacket("[1,1,5,1,1]")
+        assertThat(Day13.packetsAreInOrder(left, right)).isTrue()
+        assertThat(Day13.packetsAreInOrder(right, left)).isFalse()
+    }
+
+    @Test
+    fun `can compare packets with sublists`() {
+        val left = Day13.parsePacket("[[1],[2,3,4]]")
+        val right = Day13.parsePacket("[[1],4]")
+        assertThat(Day13.packetsAreInOrder(left, right)).isTrue()
+        assertThat(Day13.packetsAreInOrder(right, left)).isFalse()
+    }
+
+    @Test
+    fun `can get answer for part 1 using sample input`() {
+        assertThat(Day13.sumOfIndicesOfInOrderPackets(sampleInput)).isEqualTo(13)
+    }
+
+    @Test
+    fun `can get answer for part 1 using question input`() {
+        val input = Day13::class.java.getResourceAsStream("/year22/day13.txt")!!.bufferedReader().readText()
+        assertThat(Day13.sumOfIndicesOfInOrderPackets(input)).isEqualTo(4734)
+    }
+
+    private val sampleInput = """
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+
+        [[1],[2,3,4]]
+        [[1],4]
+
+        [9]
+        [[8,7,6]]
+
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+
+        [7,7,7,7]
+        [7,7,7]
+
+        []
+        [3]
+
+        [[[]]]
+        [[]]
+
+        [1,[2,[3,[4,[5,6,7]]]],8,9]
+        [1,[2,[3,[4,[5,6,0]]]],8,9]
+    """.trimIndent()
 }
