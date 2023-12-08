c833bf750e2e25e53609705174bf6059abbff5d0
jameshc2001 <jameshcrofts@gmail.com>
2023-12-08 17:41:41 +0000

    Year 23 Completed Day 6

--- src/main/kotlin/year23/Day6.kt
+++ src/main/kotlin/year23/Day6.kt
@@ -1,22 +1,42 @@
 package year23
 
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 class Day6 {
 
-    data class Race(val time: Int, val distance: Int)
+    data class Race(val time: Long, val distance: Long) {
+        fun waysToWin(): Long {
+            val temp = sqrt(((time * time) - (4 * (distance + 1))).toDouble()) //+1 to distance because we want to beat it
+            val lowest = ceil((time.toDouble() - temp) / 2).toLong()
+            val highest = floor((time.toDouble() + temp) / 2).toLong()
+            return highest - lowest + 1
+        }
+    }
 
     companion object {
 
         fun parseInput(input: String): List<Race> {
             val lines = input.lines()
             val regex = "[0-9]+".toRegex()
-            val times = regex.findAll(lines[0]).map { it.value.toInt() }
-            val distances = regex.findAll(lines[1]).map { it.value.toInt() }
+            val times = regex.findAll(lines[0]).map { it.value.toLong() }
+            val distances = regex.findAll(lines[1]).map { it.value.toLong() }
             return times.zip(distances).map { Race(it.first, it.second) }.toList()
         }
 
-        fun waysToWin(input: String): Int {
+        fun waysToWin(input: String): Long {
             val races = parseInput(input)
-            val thing = races.map { i }
+            return races.map { race -> race.waysToWin() }.reduce { acc, i -> acc * i }
+        }
+
+        fun waysToWinSingleRace(input: String): Long {
+            val races = parseInput(input)
+            val race = Race(
+                races.joinToString("") { it.time.toString() }.toLong(),
+                races.joinToString("") { it.distance.toString() }.toLong()
+            )
+            return race.waysToWin()
         }
 
 
--- src/test/kotlin/year23/Day6Test.kt
+++ src/test/kotlin/year23/Day6Test.kt
@@ -19,6 +19,23 @@ class Day6Test {
         assertThat(Day6.waysToWin(sampleInput)).isEqualTo(288)
     }
 
+    @Test
+    fun `can get answer for part 1 using question input`() {
+        val input = Day6::class.java.getResourceAsStream("/year23/day6.txt")!!.bufferedReader().readText()
+        assertThat(Day6.waysToWin(input)).isEqualTo(1731600)
+    }
+
+    @Test
+    fun `can get answer for part 2 using sample input`() {
+        assertThat(Day6.waysToWinSingleRace(sampleInput)).isEqualTo(71503)
+    }
+
+    @Test
+    fun `can get answer for part 2 using question input`() {
+        val input = Day6::class.java.getResourceAsStream("/year23/day6.txt")!!.bufferedReader().readText()
+        assertThat(Day6.waysToWinSingleRace(input)).isEqualTo(40087680)
+    }
+
     private val sampleInput = """
         Time:      7  15   30
         Distance:  9  40  200
