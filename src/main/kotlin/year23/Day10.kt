e89d7759f0dc849d7fa7de050a0a421b50dbdefd
jameshc2001 <jameshcrofts@gmail.com>
2023-12-11 12:26:33 +0000

    Year 23 Completed Day 10 Part 2

--- src/main/kotlin/year23/Day10.kt
+++ src/main/kotlin/year23/Day10.kt
@@ -39,23 +39,25 @@ class Day10 {
 
     companion object {
         fun parseInput(input: String): StartAndMap {
-            val posToPipes = input
-                .lines()
-                .map { it.replace("\n", "") }
-                .map { it.replace("\r", "") }
-                .flatMapIndexed { y: Int, line: String ->
-                    line.mapIndexedNotNull { x, c ->
-                        Pipe.fromChar(c)?.let { pipe ->
-                            Pos(x.toLong(), y.toLong()) to pipe
-                        }
-                    }
-                }
-                .toMap()
+            val posToPipes = getPosToPipes(input)
             return StartAndMap(
                 posToPipes.filterValues { pipe -> pipe == Start }.keys.single(),
                 generateMap(posToPipes)
             )
         }
+
+        private fun getPosToPipes(input: String) = input
+            .lines()
+            .map { it.replace("\n", "") }
+            .map { it.replace("\r", "") }
+            .flatMapIndexed { y: Int, line: String ->
+                line.mapIndexedNotNull { x, c ->
+                    Pipe.fromChar(c)?.let { pipe ->
+                        Pos(x.toLong(), y.toLong()) to pipe
+                    }
+                }
+            }
+            .toMap()
 
         private fun generateMap(posToPipes: Map<Pos, Pipe>): Map<Pos, Set<Pos>> {
             return posToPipes.mapValues { (pos, pipe) ->
@@ -103,6 +105,11 @@ class Day10 {
 
         fun stepsToFurthestPoint(input: String): Long {
             val (start, map) = parseInput(input)
+            val path = getLoop(start, map)
+            return path.size / 2L
+        }
+
+        private fun getLoop(start: Pos, map: Map<Pos, Set<Pos>>): MutableList<Pos> {
             val path = mutableListOf<Pos>()
             var currentPipe = start
             do {
@@ -110,7 +117,74 @@ class Day10 {
                 val neighbours = map[currentPipe]!!
                 currentPipe = if (neighbours.first() in path) neighbours.last() else neighbours.first()
             } while (currentPipe != start)
-            return path.size / 2L
+            return path
+        }
+
+        private fun getCornerPipeForStart(start: Pos, map: Map<Pos, Set<Pos>>): Pipe {
+            val neighbours = map[start]!!
+            if (neighbours.contains(Pos(start.x - 1, start.y)) && neighbours.contains(Pos(start.x, start.y + 1))) return SouthWestBend
+            if (neighbours.contains(Pos(start.x + 1, start.y)) && neighbours.contains(Pos(start.x, start.y + 1))) return SouthEastBend
+            if (neighbours.contains(Pos(start.x - 1, start.y)) && neighbours.contains(Pos(start.x, start.y - 1))) return NorthWestBend
+            if (neighbours.contains(Pos(start.x + 1, start.y)) && neighbours.contains(Pos(start.x, start.y - 1))) return NorthEastBend
+
+            throw RuntimeException("could not determine corner pipe for start")
+        }
+
+        fun enclosedArea(input: String): Int {
+            val (start, map) = parseInput(input)
+            val loop = getLoop(start, map)
+
+            val posToPipes = getPosToPipes(input).toMutableMap()
+            posToPipes[start] = getCornerPipeForStart(start, map)
+
+            val min = Pos(loop.minOf { it.x - 1 }, loop.minOf { it.y - 1 })
+            val max = Pos(loop.maxOf { it.x + 1 }, loop.maxOf { it.y + 1 })
+
+            val area = (min.x .. max.x).flatMap { x ->
+                (min.y .. max.y).map { y ->
+                    Pos(x, y)
+                }
+            }.filter { insideLoop(it, loop.toSet(), posToPipes, max.x) }
+            return area.size
+        }
+
+        private fun edgeClosesInSameDirectionAsItOpens(firstEdgePipe: Pipe, lastEdgePipe: Pipe): Boolean {
+            if (firstEdgePipe == NorthEastBend && lastEdgePipe == NorthWestBend) return true
+            if (firstEdgePipe == NorthWestBend && lastEdgePipe == NorthEastBend) return true
+            if (firstEdgePipe == SouthEastBend && lastEdgePipe == SouthWestBend) return true
+            if (firstEdgePipe == SouthWestBend && lastEdgePipe == SouthEastBend) return true
+            return false
+        }
+
+        private fun insideLoop(pos: Pos, loop: Set<Pos>, posToPipes: Map<Pos, Pipe>, maxX: Long): Boolean {
+            if (pos in loop) return false
+
+            val cornerPipes = setOf(NorthEastBend, NorthWestBend, SouthEastBend, SouthWestBend)
+            var onEdge = false
+            var firstEdgePipe: Pipe? = null
+            var currentPos = pos
+            var counter = 0
+
+            do {
+                currentPos = Pos(currentPos.x + 1, currentPos.y)
+                val currentPipe = posToPipes[currentPos]
+
+                if (onEdge) {
+                    if (currentPipe in cornerPipes) {
+                        onEdge = false
+                        if (edgeClosesInSameDirectionAsItOpens(firstEdgePipe!!, currentPipe!!)) counter++
+                    }
+                } else {
+                    if (currentPos in loop) {
+                        onEdge = currentPipe in cornerPipes
+                        firstEdgePipe = currentPipe
+                        counter++
+                    }
+                }
+
+            } while (currentPos.x <= maxX)
+
+            return (counter % 2 != 0) //odd counter means point is inside
         }
     }
 }
--- src/test/kotlin/year23/Day10Test.kt
+++ src/test/kotlin/year23/Day10Test.kt
@@ -26,12 +26,53 @@ class Day10Test {
         assertThat(Day10.stepsToFurthestPoint(input)).isEqualTo(6757)
     }
 
+    @Test
+    fun `can get answer for part 2 using simple sample input`() {
+        assertThat(Day10.enclosedArea(sampleInputPart2Simple)).isEqualTo(4)
+    }
+
+    @Test
+    fun `can get answer for part 2 using hard sample input`() {
+        assertThat(Day10.enclosedArea(sampleInputPart2)).isEqualTo(8)
+    }
+
+    @Test
+    fun `can get answer for part 2 using question input`() {
+        val input = Day10::class.java.getResourceAsStream("/year23/day10.txt")!!.bufferedReader().readText()
+        assertThat(Day10.enclosedArea(input)).isEqualTo(523)
+    }
+
     private val sampleInput = """
         7-F7-
         .FJ|7
         SJLL7
         |F--J
         LJ.LJ
+    """.trimIndent()
+
+    private val sampleInputPart2Simple = """
+        ...........
+        .S-------7.
+        .|F-----7|.
+        .||.....||.
+        .||.....||.
+        .|L-7.F-J|.
+        .|..|.|..|.
+        .L--J.L--J.
+        ...........
+    """.trimIndent()
+
+    private val sampleInputPart2 = """
+        .F----7F7F7F7F-7....
+        .|F--7||||||||FJ....
+        .||.FJ||||||||L7....
+        FJL7L7LJLJ||LJ.L-7..
+        L--J.L7...LJS7F-7L7.
+        ....F-J..F7FJ|L7L7L7
+        ....L7.F7||L7|.L7L7|
+        .....|FJLJ|FJ|F7|.LJ
+        ....FJL-7.||.||||...
+        ....L---J.LJ.LJLJ...
     """.trimIndent()
 
 }
