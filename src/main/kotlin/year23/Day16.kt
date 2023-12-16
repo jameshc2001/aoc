4ddf2ca427626b1a2b519cc48d2d170aed27bde5
jameshc2001 <jameshcrofts@gmail.com>
2023-12-16 11:26:08 +0000

    Year 2023 Completed Day 16 Part 1

---
+++ src/main/kotlin/year23/Day16.kt
@@ -0,0 +1,90 @@
+package year23
+
+class Day16 {
+    data class Pos(val x: Int, val y: Int) {
+        operator fun plus(other: Pos) = Pos(x + other.x, y + other.y)
+    }
+    data class BeamHead(val pos: Pos, val direction: Pos)
+
+    @JvmInline
+    value class Layout(val map: Map<Pos, Char>)
+
+    companion object {
+        fun parseInput(input: String): Layout = Layout(
+            input.lines()
+                .map { it.replace("\n", "").replace("\r", "") }
+                .flatMapIndexed { y, line ->
+                    line.mapIndexed { x, c -> Pos(x, y) to c }
+                }.toMap()
+        )
+
+        private fun beamHeadStep(beamHead: BeamHead, layout: Layout): List<BeamHead> {
+            val (pos, direction) = beamHead
+            val nextPos = pos + direction
+            val nextTile = layout.map[nextPos] ?: return emptyList()
+
+            val resultingBeamHeads = when (nextTile) {
+                '.' -> listOf(beamHead.copy(pos = nextPos))
+                '-' -> if (direction.y == 0) {
+                    listOf(beamHead.copy(pos = nextPos))
+                } else {
+                    listOf(BeamHead(nextPos, Pos(1, 0)), BeamHead(nextPos, Pos(-1, 0)))
+                }
+                '|' -> if (direction.x == 0) {
+                    listOf(beamHead.copy(pos = nextPos))
+                } else {
+                    listOf(BeamHead(nextPos, Pos(0, 1)), BeamHead(nextPos, Pos(0, -1)))
+                }
+                '\\' -> when(direction) {
+                    Pos(1, 0) -> listOf(BeamHead(nextPos, Pos(0, 1)))
+                    Pos(-1, 0) -> listOf(BeamHead(nextPos, Pos(0, -1)))
+                    Pos(0, 1) -> listOf(BeamHead(nextPos, Pos(1, 0)))
+                    Pos(0, -1) -> listOf(BeamHead(nextPos, Pos(-1, 0)))
+                    else -> null
+                }
+                '/' -> when(direction) {
+                    Pos(1, 0) -> listOf(BeamHead(nextPos, Pos(0, -1)))
+                    Pos(-1, 0) -> listOf(BeamHead(nextPos, Pos(0, 1)))
+                    Pos(0, 1) -> listOf(BeamHead(nextPos, Pos(-1, 0)))
+                    Pos(0, -1) -> listOf(BeamHead(nextPos, Pos(1, 0)))
+                    else -> null
+                }
+                else -> null
+            }
+
+            return resultingBeamHeads ?: throw RuntimeException("could not calculate resulting beam heads")
+        }
+
+        fun energisedTiles(input: String): Int {
+            val layout = parseInput(input)
+            var beamHeads = beamHeadStep(BeamHead(Pos(-1, 0), Pos(1, 0)), layout)
+            val energisedTiles = mutableSetOf<Pos>()
+            val historicBeamHeads = mutableSetOf<BeamHead>()
+
+            while (beamHeads.isNotEmpty()) {
+                historicBeamHeads.addAll(beamHeads)
+                beamHeads = beamHeads.flatMap { beamHead ->
+                    energisedTiles.add(beamHead.pos) //assume we are on valid tile
+                    beamHeadStep(beamHead, layout).filter { it !in historicBeamHeads }
+                }
+            }
+
+            return energisedTiles.size
+        }
+
+        fun print(layout: Layout, energisedTiles: Set<Pos>) {
+            val max = Pos(layout.map.keys.maxOf { it.x }, layout.map.keys.maxOf { it.y })
+            println()
+            (0 .. max.y).forEach { y ->
+                (0 .. max.x).forEach { x ->
+                    val pos = Pos(x, y)
+                    val char = if (pos in energisedTiles) '#' else layout.map[pos]!!
+                    print(char)
+                }
+                println()
+            }
+        }
+    }
+
+
+}
---
+++ src/test/kotlin/year23/Day16Test.kt
@@ -0,0 +1,42 @@
+package year23
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import year23.Day16.Pos
+
+class Day16Test {
+
+    @Test
+    fun `can parse sample input`() {
+        val layout = Day16.parseInput(sampleInput)
+        assertThat(layout.map).containsEntry(Pos(1, 0), '|')
+        assertThat(layout.map).containsEntry(Pos(2, 1), '-')
+        assertThat(layout.map).containsEntry(Pos(4, 1), '\\')
+        assertThat(layout.map).containsEntry(Pos(3, 9), '/')
+    }
+
+    @Test
+    fun `can get answer for part 1 using sample input`() {
+        assertThat(Day16.energisedTiles(sampleInput)).isEqualTo(46)
+    }
+
+    @Test
+    fun `can get answer for part 1 using question input`() {
+        val input = Day16::class.java.getResourceAsStream("/year23/day16.txt")!!.bufferedReader().readText()
+        assertThat(Day16.energisedTiles(input)).isEqualTo(7236)
+    }
+
+    private val sampleInput = """
+        .|...\....
+        |.-.\.....
+        .....|-...
+        ........|.
+        ..........
+        .........\
+        ..../.\\..
+        .-.-/..|..
+        .|....-|.\
+        ..//.|....
+    """.trimIndent()
+
+}
