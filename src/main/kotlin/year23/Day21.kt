7cc2670d9ce5a92b03bc381d116a28eb5fba4a95
jameshc2001 <jameshcrofts@gmail.com>
2023-12-23 17:03:02 +0000

    Year 23 Day 21 Completed

---
+++ src/main/kotlin/year23/Day21.kt
@@ -0,0 +1,69 @@
+package year23
+
+class Day21 {
+
+    data class Pos(val x: Long, val y: Long) {
+        fun adjacent() = listOf(
+            Pos(x - 1, y),
+            Pos(x + 1, y),
+            Pos(x, y - 1),
+            Pos(x, y + 1),
+        )
+        operator fun minus(other: Pos) = Pos(x - other.x, y - other.y)
+    }
+
+    companion object {
+        fun parseInput(input: String): Pair<Pos, Set<Pos>> {
+            var start: Pos? = null
+            val garden = input.lines().map { it.replace("\n", "").replace("\r", "") }
+                .flatMapIndexed { y, line ->
+                    line.mapIndexedNotNull { x, c ->
+                        val pos = Pos(x.toLong(), y.toLong())
+                        if (c == 'S') start = pos
+                        if (c == '.' || c == 'S') pos else null
+                    }
+                }
+            return start!! to garden.toSet()
+        }
+
+        fun reachableGardens(input: String, steps: Int): Int {
+            val (start, gardens) = parseInput(input)
+            var reachable = setOf(start)
+            repeat(steps) {
+                reachable = reachable.map { garden -> garden.adjacent().filter { it in gardens } }.flatten().toSet()
+            }
+            return reachable.size
+        }
+
+        fun wrapAroundReachableGardens(start: Pos, gardens: Set<Pos>, size: Long, steps: Int): Int { //assume square
+            var reachable = setOf(start)
+            repeat(steps) {
+                reachable = reachable.map { garden ->
+                    garden.adjacent().filter {
+                        Pos(it.x.mod(size), it.y.mod(size)) in gardens
+                    } }.flatten().toSet()
+            }
+            return reachable.size
+        }
+
+        fun infiniteReachableGardens(input: String, steps: Long): Long {
+            val (start, gardens) = parseInput(input)
+            val size = gardens.maxOf { it.x } + 1
+            val offset = (size / 2).toInt()
+            println(offset)
+            println(size)
+            val y1 = wrapAroundReachableGardens(start, gardens, size, offset).toLong()
+            val y2 = wrapAroundReachableGardens(start, gardens, size, size.toInt() + offset).toLong()
+            val y3 = wrapAroundReachableGardens(start, gardens, size, size.toInt() * 2 + offset).toLong()
+            println(y1)
+            println(y2)
+            println(y3)
+
+            val y = 202300
+            val what = 620348631910321
+
+            val x = steps
+            return y1 * (x - 1) * (x - 2) / 2 - y2 * x * (x - 2) + y3 * x * (x - 1) / 2
+        }
+    }
+}
---
+++ src/test/kotlin/year23/Day21Test.kt
@@ -0,0 +1,55 @@
+package year23
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class Day21Test {
+
+    @Test
+    fun `can parse the sample input`() {
+        val (start, gardens) = Day21.parseInput(sampleInput)
+        assertThat(start).isEqualTo(Day21.Pos(5, 5))
+        assertThat(gardens).contains(Day21.Pos(10, 10))
+        assertThat(gardens).contains(Day21.Pos(0, 0))
+    }
+
+    @Test
+    fun `can get answer for part 1 using sample input`() {
+        assertThat(Day21.reachableGardens(sampleInput, 6)).isEqualTo(16)
+    }
+
+    @Test
+    fun `can get answer for part 1 using question input`() {
+        val input = Day21::class.java.getResourceAsStream("/year23/day21.txt")!!.bufferedReader().readText()
+        assertThat(Day21.reachableGardens(input, 64)).isEqualTo(3724)
+    }
+
+    @Test
+    fun `can get wrap around reachable gardens`() {
+        val (start, gardens) = Day21.parseInput(sampleInput)
+        val size = gardens.maxOf { it.x } + 1
+        assertThat(Day21.wrapAroundReachableGardens(start, gardens, size, 10)).isEqualTo(50)
+        assertThat(Day21.wrapAroundReachableGardens(start, gardens, size, 50)).isEqualTo(1594)
+    }
+
+    @Test
+    fun `can get answer for part 2 using question input`() {
+        val input = Day21::class.java.getResourceAsStream("/year23/day21.txt")!!.bufferedReader().readText()
+        assertThat(Day21.infiniteReachableGardens(input, 0)).isEqualTo(1594)
+    }
+
+    private val sampleInput = """
+        ...........
+        .....###.#.
+        .###.##..#.
+        ..#.#...#..
+        ....#.#....
+        .##..S####.
+        .##..#...#.
+        .......##..
+        .##.#.####.
+        .##..##.##.
+        ...........
+    """.trimIndent()
+
+}
